dbf69de91e5db14de440d192a41dff1ad2de1c9e
nyxiereal <98595166+nyxiereal@users.noreply.github.com>
2026-04-20 10:30:00 +0200

    bump

--- uets.meta.js
+++ uets.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Universal Educational Tool Suite
 // @namespace    http://tampermonkey.net/
-// @version      1.6.1
+// @version      1.6.2
 // @description  A unified tool for cheating on online test sites
 // @author       Nyx
 // @license      GPL-3.0
